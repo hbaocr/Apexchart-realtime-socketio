7247d418e834f54a79b391741d914dce893efda3
Duong Huynh Bao <duonghuynhbao@MacBook-Pro-3.local>
2020-07-14 22:25:28 +0700

    add slide speed

--- web/index.js
+++ web/index.js
@@ -1,67 +1,91 @@
 
 
-var data = []
+
 var TICKINTERVAL = 86400000
 let XAXISRANGE = 777600000
-
 let chart;
 var socket = io();
-function shift(arr, direction, n) {
-    var times = n > arr.length ? n % arr.length : n;
-    return arr.concat(arr.splice(0, (direction > 0 ? arr.length - times : times)));
- }
-
-function render_chart(new_dat) {
-    try {
-
-        if (chart) {
-            data = [...data, ...new_dat];
-            chart.updateSeries([{
-                data: data
-            }])
-        }
-    } catch (error) {
-
-    }
-}
-
-
-function render_chart_window(new_dat,window_size) {
-    try {
-
-        if (chart) {
-            data = [...data, ...new_dat];
-            if(data.length>window_size){
-                //let rm = data.length - window_size;
-               // data = shift(data,0,rm);
-               data.shift();
-            }
-            chart.updateSeries([{
-                data: data
-            }])
-        }
-    } catch (error) {
-
-    }
-}
-
-
-
 const { fromEvent, Observable, of, interval } = rxjs;
-const { map, mergeMap, delay, bufferTime, concatAll,concatMap } = rxjs.operators;
+const { map, mergeMap, delay, bufferTime, concatAll, concatMap } = rxjs.operators;
+let wd_size = 200;
+let slide_speed=2;
+let pool_data = [];
 
+let window_data = [];
+let t_render = 100;
 const observable = fromEvent(socket, 'newmsg');
 observable.pipe(
     mergeMap(json => JSON.parse(json)),// flatmap 
-    concatMap(x=>of(x).pipe(delay(40))),//delay each element in 20ms
+    concatMap(x => of(x).pipe(delay(20))),//delay each element in 20ms
     //bufferTime(100),//buffer untill 100ms then emit the array
 ).subscribe((msg) => {
-    console.log(new Date().getTime()+ '  |  ' +msg);
-    //render_chart([msg]);
-    render_chart_window([msg],200);
+    //console.log('--->event new msg'+ new Date().getTime() +JSON.stringify(msg));
+    pool_data.push(msg);
 });
 
+let render_dat = [];
 
+
+function shift_one(window_size = wd_size) {
+    // move 1 point
+    let dat = pool_data.shift();
+    window_data = [...window_data, dat];
+    if (window_data.length > window_size) {
+        window_data.shift();
+    }
+}
+
+function shift_n(speed_n,window_size = wd_size) {
+    // move 1 point
+    let dat = pool_data.splice(0,speed_n);
+    window_data.splice(0,speed_n);
+    window_data = [...window_data, ...dat];
+   
+}
+
+
+function slide_window_render(speed=2,window_size) {
+    let lead_num = pool_data.length;
+
+    if (chart) {
+        if (lead_num > 0) {
+
+            if (window_data.length < window_size) {
+                shift_one(window_size);// move 1 point
+                //shift_n(speed)
+            } else { // full window
+
+                let dist = lead_num - window_size + 1;
+                if (dist <= window_size) {                    
+                    shift_n(speed);
+                } else {
+                    dist = window_data.length / 3;
+          
+                    shift_n(Math.floor(dist));
+
+                }
+            }
+            // chart.setData(window_data);
+            chart.updateSeries([{
+                data: window_data
+            }])
+        }
+
+
+    }
+
+}
+
+
+
+function period_render(t_render) {
+    setTimeout(async () => {
+        slide_window_render(slide_speed,wd_size);
+        console.log(`${new Date().getTime()} t_render : ${t_render}, window_size:${window_data.length} ,lead_num: ${pool_data.length}`);
+        period_render(t_render);
+    }, t_render);
+}
+period_render(t_render)
 
 
 
@@ -70,7 +94,7 @@ window.onload = () => {
     const el = document.getElementById('chart');
     var options = {
         series: [{
-            data: data.slice()
+            data: []
         }],
         chart: {
             id: 'realtime',
